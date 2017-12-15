4822298c83c8e75c074d29d44ca5f04179b3a850
eitanp82 <eitanperetz@gmail.com>
2017-12-15 15:41:15 +0200

    Add triangle shape.

--- shapes/__init__.py
+++ shapes/__init__.py
@@ -1,6 +1,7 @@
 from .circle import Circle
 from .rectangle import Rectangle
 from .square import Square
+from .triangle import Triangle
 
-__all__ = ['Circle', 'Rectangle', 'Square']
+__all__ = ['Circle', 'Rectangle', 'Square', 'Triangle']
 
