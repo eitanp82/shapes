c920eec9e0c533b7548dc604b307cdedc9d92e97
eitanp82 <eitanperetz@gmail.com>
2017-12-15 16:28:57 +0200

    Remove custom install command.

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
 from setuptools import find_packages, setup
-# from distutils.core import setup
-from distutils.command.clean import clean
-from distutils.command.install import install as BaseInstall
-
-
-class Install(BaseInstall):
-    def run(self):
-        # run install command.
-        BaseInstall.run(self)
-        # run post-install command.
-        self.post_install()
-
-    def post_install(self):
-        self.clean_build()
-
-    def clean_build(self):
-        c = clean(self.distribution)
-        c.all = True
-        c.finalize_options()
-        c.run()
 
 
 setup(
@@ -30,6 +10,5 @@ setup(
     packages=find_packages(),
     install_requires=['six', 'test'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    cmdclass={'install': Install}
+    tests_require=['pytest']
 )
