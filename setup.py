9130f883f96cec7d542e2455d4744f42d1d1e4ba
eitanp82 <eitanperetz@gmail.com>
2017-12-15 12:10:16 +0200

    Add shapes package

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+# from distutils.core import setup
+from distutils.command.clean import clean
+from distutils.command.install import install as BaseInstall
+
+
+class Install(BaseInstall):
+    def run(self):
+        # run install command.
+        BaseInstall.run(self)
+        # run post-install command.
+        self.post_install()
+
+    def post_install(self):
+        self.clean_build()
+
+    def clean_build(self):
+        c = clean(self.distribution)
+        c.all = True
+        c.finalize_options()
+        c.run()
+
+
+setup(
+    name='shapes',
+    version='1.0',
+    description='Python shapes',
+    author='Eitan Peretz',
+    author_email='eitanperetz@gmail.com',
+    packages=find_packages(),
+    install_requires=['six', 'test'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    cmdclass={'install': Install}
+)
---
+++ shapes/__init__.py
@@ -0,0 +1,6 @@
+from .circle import Circle
+from .rectangle import Rectangle
+from .square import Square
+
+__all__ = ['Circle', 'Rectangle', 'Square']
+
